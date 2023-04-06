a9796b4e12f9eac7ac3a152c03c146e746ae1521
Jac22 <jacopo.prudentino@gmail.com>
2023-04-06 15:40:42 +0200

    Created basic functions

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
-﻿void StampaArray(int[] Array)
+﻿void StampaArray(int[] numbers)
 {
     Console.Write("[");
-    for (int i = 0; i < Array.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        Console.Write(Array[i]);
-        if (i < Array.Length - 1)
+        Console.Write(numbers[i]);
+        if (i < numbers.Length - 1)
             Console.Write(',');
     }
     Console.WriteLine("]");
+}
+
+int Quadrato(int n)
+{
+    return n * n;
+}
+
+int[] ElevaArrayAlQuadrato(int[] Numbers)
+{
+    int[] CloneArr = (int[])Numbers.Clone();
+
+    for (int i = 0; i < CloneArr.Length; i++)
+        CloneArr[i] = Quadrato(CloneArr[i]);
+
+    return CloneArr;
+}
+
+int sommaElementiArray(int[] numbers)
+{
+    int somma = 0;
+    for (int i = 0;i < numbers.Length; i++) 
+        somma += numbers[i];
+    return somma;
 }
