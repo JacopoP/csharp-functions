acc3ab79cf70e0088177b395c4a5b07b7dc7d75a
Jac22 <jacopo.prudentino@gmail.com>
2023-04-06 15:33:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿void StampaArray(int[] Array)
+{
+    Console.Write("[");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i]);
+        if (i < Array.Length - 1)
+            Console.Write(',');
+    }
+    Console.WriteLine("]");
+}
